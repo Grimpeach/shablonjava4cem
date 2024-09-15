6f81a01078890ef17d9e87d072a1c9721a7c4259
Grimpeach <106539421+Grimpeach@users.noreply.github.com>
2024-09-15 14:03:06 +0300

    Update Game.java

--- practice8/Template/Game.java
+++ practice8/Template/Game.java
@@ -1,14 +1,13 @@
 package practice8.Template;
 
 abstract class Game {
-    // Шаблонный метод, который определяет общий алгоритм
+    
     public final void play() {
         initialize();
         startPlay();
         endPlay();
     }
 
-    // Методы, которые нужно реализовать в подклассах
     abstract void initialize();
     abstract void startPlay();
     abstract void endPlay();
